e707dcfab2eb0438132c527a4c8c27863318ef1c
Paula <paula.batoon@gmail.com@users.noreply.github.com>
2020-05-30 21:51:51 -0500

    First commit

---
+++ src/Game.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+
+public class Game {
+
+    public static void main(String[] args) {
+
+        //Declare HashMap -> Character, String pairs
+        HashMap<Character, String> natoAlphabet = new HashMap<Character, String>() {{
+            put('a', "alfa");
+            put('b', "bravo");
+            put('c', "charlie");
+            put('d', "delta");
+            put('e', "echo");
+            put('f', "foxtrot");
+            put('g', "golf");
+            put('h', "hotel");
+            put('i', "india");
+            put('j', "juliett");
+            put('k', "kilo");
+            put('l', "lima");
+            put('m', "mike");
+            put('n', "november");
+            put('o', "oscar");
+            put('p', "papa");
+            put('q', "quebec");
+            put('r', "romeo");
+            put('s', "sierra");
+            put('t', "tango");
+            put('u', "uniform");
+            put('v', "victor");
+            put('w', "whiskey");
+            put('x', "x-ray");
+            put('y', "yankee");
+            put('z', "zulu");
+        }};
+
+        /*
+        //Print entire natoAlphabet hashmap
+        for (char i : natoAlphabet.keySet()) {
+            System.out.println("key: " + i + " value: " + natoAlphabet.get(i));
+        }
+        */
+
+
+
+        //System.out.println("\nRandom value: " + key + " :: " + natoAlphabet.get(key));
+
+        List<Object> letters = new ArrayList<>();
+
+        int i = 0;
+        while(i <= natoAlphabet.size()) {
+            //grab random value from Hashmap
+            Object[] natoKeys = natoAlphabet.keySet().toArray();
+            Object key = natoKeys[new Random().nextInt(natoKeys.length)];
+
+            if(letters.contains(key)) {
+                key = natoKeys[new Random().nextInt(natoKeys.length)];
+            } else {
+
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter code word for the letter " + key + " : ");
+
+                String answer = (scanner.nextLine()).toLowerCase();
+
+                if (natoAlphabet.get(key).equals(answer)) {
+                    System.out.println("Correct! " + answer + " is the word for " + key);
+                } else {
+                    System.out.println("Incorrect.");
+                }
+
+                letters.add(key);
+                i++;
+            }
+        }
+
+    }
+}
